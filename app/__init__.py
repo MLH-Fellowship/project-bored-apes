aada6c76df4c88583290aeb81214b56aa495155c
Logan Hoogendijk <logan.hoogendijk@gmail.com>
2022-06-01 17:06:02 -0400

    Added v1 of hobbies page, subject to change

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/hobbies/')
+def hobbies():
+    return render_template('hobbies.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
