93e6f65cec254f257b16b7e0faeedf1cde452763
Logan Hoogendijk <logan.hoogendijk@gmail.com>
2022-06-02 10:26:01 -0400

    Updated hobbies HTML page to match the template

--- app/__init__.py
+++ app/__init__.py
@@ -12,8 +12,20 @@ def index():
 
 @app.route('/hobbies/')
 def hobbies():
-    return render_template('hobbies.html')
+    hobbies = [
+        {
+            "image": "https://media.timeout.com/images/105863223/750/422/image.jpg",
+            "hobby": "Star Wars"
+        }, {
+            "image": "https://hips.hearstapps.com/amv-prod-gp.s3.amazonaws.com/gearpatrol/wp-content/uploads/2019/11/Mechanical-Keyboard-Buying-Guide-Gear-Patrol-Feature.jpg?crop=1xw:0.65xh;center,top&resize=1200:*",
+            "hobby": "Mechanical Keyboards"
+        }, {
+            "image": "https://assets-prd.ignimgs.com/2021/12/30/36190303-image-7-1640880187142.png",
+            "hobby": "Video Games"
+        }
+        
+    ]
+    return render_template('hobbies.html', hobbies=hobbies)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
