ca1de36440a69d7937882be6e8dd4e20d86dbe77
HadiChaaban <chaabanh@umich.edu>
2022-06-03 03:50:56 -0400

    Navbar fix and about me design

--- app/static/js/arrow.js
+++ app/static/js/arrow.js
@@ -1,10 +1,14 @@
 var a;
 function show_hide(){
     if (a == 1){
-        document.getElementById("topnavbar").style.display="block";
+        document.getElementById("topnavbar").style.visibility="visible";
+        document.getElementById("grnav").className="navbar";
+        document.getElementById("btnarrow").innerHTML="<span class='icon'><i class='fa-solid fa-angle-up'></i></span>";
         return a=0;
     } else {
-        document.getElementById("topnavbar").style.display="none";
+        document.getElementById("topnavbar").style.visibility="hidden";
+        document.getElementById("grnav").className="is-transparent";
+        document.getElementById("btnarrow").innerHTML="<span class='icon'><i class='fa-solid fa-angle-down'></i></span>";
         return a=1;
     }
 }
