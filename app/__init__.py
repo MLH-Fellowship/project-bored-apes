dfe01c5497b5c8efeea583485389b10321014c50
Justin Monteza <26825560+jmonteza@users.noreply.github.com>
2022-06-05 01:32:35 -0600

    Added 404 and styling changes

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ from http.client import HTTPResponse
 import os
 import re
 import json
-from flask import Flask, render_template, request, url_for, redirect
+from tkinter import ANCHOR
+from flask import Flask, render_template, request, url_for, redirect, Response
 from dotenv import load_dotenv
 from .gmail.gmail import send_email
 
@@ -15,12 +16,10 @@ with open(filename) as f:
     data = json.load(f)
 
 
-@app.route("/<any(logan, hadi, justin):name>")
+@app.route("/<any(logan, hadi, justin):name>", methods=['GET'])
 def index(name):
     anchors = ["Experience", "Projects", "Hobbies", "Map", "Contact"]
-
     return render_template('index.html', title=name, url=os.getenv("URL"), data=data, anchors=anchors)
-
 
 @app.route("/contact", methods=['POST'])
 def contact():
@@ -29,26 +28,14 @@ def contact():
         sender_email = request.form.get("email")
         message = request.form.get("message")
         subject = request.form.get("subject")
-
         receiver_name = request.form.get("receiver_name")
         receiver_email = request.form.get("receiver_email")
-
         formatted_message = "Name: {}\nEmail: {}\nMessage: {}".format(sender_name, sender_email, message)
-        
-        send_email("Justin Monteza", "justin.monteza@gmail.com", subject, formatted_message)
-
+        send_email(receiver_name, receiver_email, subject, formatted_message)
         return '',204
 
-        # return None
 
-# @app.route('/logan')
-# def index():
-#     return render_template('index.html', title='logan', url=os.getenv("URL"), data=data)
-
-# @app.route('/hadi')
-# def second():
-#     return render_template('index.html', title='hadi', url=os.getenv("URL"), data=data)
-
-# @app.route('/justin')
-# def third():
-#     return render_template('index.html', title='justin', url=os.getenv("URL"), data=data)
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
