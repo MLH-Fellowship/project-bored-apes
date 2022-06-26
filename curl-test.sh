adb44ae0675e674b006bcf9caaeb1841b5b13a03
Justin Monteza <26825560+jmonteza@users.noreply.github.com>
2022-06-26 01:15:35 -0600

    Update curl-test.sh

--- curl-test.sh
+++ curl-test.sh
@@ -1,20 +1,22 @@
-# Get random lorem ipsum
+#!/bin/bash
 
+# Get random lorem ipsum
 content=$(curl -s http://asdfast.beobit.net/api/ | jq -r '.["text"]')
 
-# echo $content
+# Post data with name, email, and lorem ipsum content
+id=$(curl -s --request POST http://localhost:5000/api/timeline_post -d "name=Justin&email=justin.monteza@gmail.com&content=$(echo "$content")" | jq -r '.["id"]')
 
-# curl -s http://localhost:5000/api/timeline_post | jq -r '.["timeline_posts"][] | {id: .id}'
+# Get the post we just added using its id
+check=$(curl -s http://localhost:5000/api/timeline_post | jq -r '[.["timeline_posts"][] | .id] | @sh' | grep $id)
 
-# curl -s http://localhost:5000/api/timeline_post | jq -r '[.["timeline_posts"][] | .id]' | grep 7
-
-# Post Data
-id=$(curl --request POST http://localhost:5000/api/timeline_post -d "name=Justin&email=justin.monteza@gmail.com&content=$(echo "$content")" | jq -r '.["id"]')
-
-echo $id
-
-# Get 
-curl -s http://localhost:5000/api/timeline_post | jq -r '[.["timeline_posts"][] | .id] | @sh' | grep $id
+# Check must be non-empty
+if [ "$check" ]
+then 
+    echo "Successfully added a timeline post with id $id"
+# We cannot find the post, therefore it failed to add
+else
+    echo "Failed to add a timeline post"
+fi
 
 # Sends a delete request
 curl -X DELETE http://localhost:5000/api/timeline_post/$id
