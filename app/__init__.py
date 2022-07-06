59efd5ffb4da803e83b69d5db0515cdbf3edcc8d
HadiCya <87607836+HadiCya@users.noreply.github.com>
2022-07-06 15:40:11 -0400

    Revert "API additions"

--- app/__init__.py
+++ app/__init__.py
@@ -4,34 +4,9 @@ import json
 from flask import Flask, render_template, request, url_for, redirect, Response
 from dotenv import load_dotenv
 from .gmail.gmail import send_email
-from peewee import *
-from datetime import datetime
-from playhouse.shortcuts import model_to_dict
-
 
 load_dotenv()
 app = Flask(__name__)
-
-mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
-    user=os.getenv("MYSQL_USER"),
-    host=os.getenv("MYSQL_HOST"),
-    password=os.getenv("MYSQL_PASSWORD"),
-    port=3306
-)
-print(mydb)
-
-class TimelinePost(Model):
-    name = CharField()
-    email = CharField()
-    content = TextField()
-    created_at = DateTimeField(default=datetime.now)
-
-    class Meta:
-        database = mydb
-
-mydb.connect()
-mydb.create_tables([TimelinePost])
-
 
 data = 0
 filename = os.path.join(app.static_folder, 'data.json')
@@ -64,21 +39,3 @@ def contact():
 def page_not_found(e):
     # Set the 404 status explicitly
     return render_template('pages/404.html'), 404
-
-@app.route('/api/timeline_post', methods=['POST'])
-def post_time_line_post():
-    name = request.form['name']
-    email = request.form['email']
-    content = request.form['content']
-    timeline_post = TimelinePost.create(name=name, email=email, content=content)
-
-    return model_to_dict(timeline_post)
-
-@app.route('/api/timeline_post', methods=['GET'])
-def get_time_line_post():
-    return{
-        'timeline_posts': [
-            model_to_dict(p)
-            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
-        ]
-    }
--- curl-test.sh
+++
@@ -1,4 +0,0 @@
-
-curl --request POST http://localhost:5000/api/timeline_post -d 'name=Hadi&email=chaabanhadi22@gmail.com&content=First email!'
-wait
-curl --request GET http://localhost:5000/api/timeline_post
